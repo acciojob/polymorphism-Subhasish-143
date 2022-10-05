223653acd85651e1f7b8dcd14f85189b58fffc81
subhasish <40573716+subha143@users.noreply.github.com>
2022-10-05 13:50:10 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-
+	class Product {
+		public int product(int x,int y) {
+			return x*y;
+		}
+		public int product(int x,int y,int z) {
+			return x*y*z;
+		}
+		public double product(double x,double y) {
+			return x*y;
+		}
+	}
+	publiuc static void main(String [] args) {
+		Product p = new Product();
+		p.product(10,20);
+		p.product(10,20,30);
+		p.product(0.5,2.8);
 }
